f2e0a0c2c36e0434433f826e1f12d444186b2356
Ajoy10 <achu.ajoy10@gmail.com>
2022-12-29 11:59:12 +0530

    Added CTA in Hero section

--- portfolio/src/sections/Hero/Hero.jsx
+++ portfolio/src/sections/Hero/Hero.jsx
@@ -1,3 +1,4 @@
+import { Icon } from "@iconify/react";
 import React from "react";
 import "./style.scss";
 
@@ -12,6 +13,24 @@ export default function Hero() {
         <span id="highlight">Full Stack Web Developer</span>
         <br />
       </h1>
+      <div className="cta">
+        <div className="buttons">
+          <a href="#" className="button primary">
+            Projects
+          </a>
+          <a href="#" className="button secondary">
+            Contact
+          </a>
+        </div>
+        <div className="socials">
+          <a href="#" className="button social">
+            <Icon icon="ant-design:linkedin-filled" height="36" />
+          </a>
+          <a href="#" className="button social">
+            <Icon icon="ant-design:github-filled" height="36" />
+          </a>
+        </div>
+      </div>
     </main>
   );
 }
