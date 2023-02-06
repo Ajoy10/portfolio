10d04ad214395ff9383fa8dcaef721a009ef8d25
Ajoy10 <achu.ajoy10@gmail.com>
2023-02-06 19:29:21 +0530

    Added favicons, Added Netlify form setup

--- portfolio/src/sections/Contact/Contact.jsx
+++ portfolio/src/sections/Contact/Contact.jsx
@@ -10,25 +10,25 @@ export default function Contact() {
         <div className="input-text-group">
           <div className="input-group">
             <label htmlFor="first-name">First name</label>
-            <input type="text" name="first-name" />
+            <input type="text" name="first-name" id="first-name" />
           </div>
 
           <div className="input-group">
             <label htmlFor="last-name">Last name</label>
-            <input type="text" name="last-name" />
+            <input type="text" name="last-name" id="last-name" />
           </div>
         </div>
         <div className="input-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" id="email" />
         </div>
         <div className="input-group">
           <label htmlFor="subject">Subject</label>
-          <input type="text" name="subject" />
+          <input type="text" name="subject" id="subject" />
         </div>
         <div className="input-group">
           <label htmlFor="message">Message</label>
-          <textarea />
+          <textarea name="message" id="message" />
         </div>
         <div className="button-group">
           <button type="submit" className="button primary">
